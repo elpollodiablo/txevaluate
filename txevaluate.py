e347250feb6e391a0829422795095a338ea0ad3e
Philip Poten <philip@poten.at>
2016-01-16 19:39:34 +0000

    operator fixes

--- txevaluate.py
+++ txevaluate.py
@@ -2,58 +2,75 @@ from twisted.internet import defer
 from twisted.python import log
 
 def OR(*args):
-    l = ['||']
+    l = ['_op_or']
     l.extend(args)
     return l
     
 def AND(*args):
-    l = ['&&']
+    l = ['_op_and']
     l.extend(args)
     return l
 
 def NOT(arg):
-    return ['!', arg]
+    return ['_op_not', arg]
 
-def TRUE():
-    return ['true']
+def TRUE(*args):
+    return ['_op_true']
 
-def FALSE():
-    return ['false']
+def FALSE(*args):
+    return ['_op_false']
 
 def ELEMENT_OF(el, el_l):
-    return ['E', el, el_l]
+    return ['_op_elof', el, el_l]
+
+def EQUAL(el1, el2):
+    return ["_op_equal", el1, el2]
 
 class Evaluator():
     ops = {}
     def __init__(self, dynamic_args ={}, fixed_value_args = {}):
+        #print "evaluator init"
         self.dynamic_args = dynamic_args
         self.fixed_value_args = fixed_value_args
 
     def _translate_arg(self, arg):
-         translated_arg = self.dynamic_args.get(
+        #print "translate", arg
+        translated_arg = self.dynamic_args.get(
              str(arg),
              lambda *ignored_args, **ignored_kws:
                  self.fixed_value_args.get(str(arg), arg))()
-         if (isinstance(translated_arg, defer.Deferred)
-             or isinstance(translated_arg, defer.DeferredList)):
-             return translated_arg
-         else:
-             # a deferredlist only accepts deferreds - make this one
-             return defer.succeed(translated_arg)
+        if (isinstance(translated_arg, defer.Deferred)
+            or isinstance(translated_arg, defer.DeferredList)):
+            return translated_arg
+        elif type(translated_arg) == type(list()) and hasattr(self, arg[0]):
+            #print "seems to be an operator too", translated_arg
+            return self._call_op_real(translated_arg[0], *translated_arg[1:])
+        else:
+            # a deferredlist only accepts deferreds - make this one
+            return defer.succeed(translated_arg)
 
     def _translate_args(self, args):
         return [_translate_arg(arg) for arg in args]
 
-    def _call_op(self, expression):
-        if isinstance(expression, bool):
-            return expression
+    def _call_op_real(self, op, *args):
+        #print "call_op_real", op, args
+        if op in (True, False, None):
+            return defer.succeed(op or False)
 
+        if hasattr(self, op):
+            return getattr(self, op)(*args)
+        else:
+            log.err("no such operand: %s" % op)
+            raise Exception()
+    def _call_op(self, expression):
+        if expression in (True, False, None):
+            return defer.succeed(expression or False)
         def handle_expression_cb(result):
-            if isinstance(result, bool):
-                return result
+            if result in (True, False, None):
+                return defer.succeed(result or False)
             op = result[0]
             args = result[1:]
-            return self.ops[op](self, * args)            
+            return self._call_op_real(op, *args)            
         # two cases:
         # a) we get a real expression (a list)
         # b) we get a deferred. deal with both!
@@ -63,11 +80,16 @@ class Evaluator():
         else:
             op = expression[0]
             args = expression[1:]
-            return self.ops[op](self, * args)
+            return self._call_op_real(op, *args)
             
     def _op_not(self, expression):
-        return not self._call_op(self._translate_arg(expression))
-    ops["!"] = _op_not
+        #print "not", expression
+		def op_not_cb(expression):
+			#print "in not returning", expression, not expression
+			return not expression
+		expression_d = self._call_op(self._translate_arg(expression))
+		expression_d.addCallback(op_not_cb)
+		return expression_d
 
     def _op_or(self, *expressions):
         def or_cb(result, expressions):
@@ -85,26 +107,29 @@ class Evaluator():
                 call_result_d.addCallback(or_cb, remaining_exp)
                 return call_result_d
         return or_cb(False, expressions)
-    ops["||"] = _op_or
 
     def _op_and(self, *expressions):
         def and_cb(result, expressions):
+            #print "result and expressions", result, expressions
             if not result:
+                #print "not result, False"
                 return False
             elif not expressions:
                 # we're the last result - if result is not false,
                 # no remaining means...
+                #print "not expressions, True"
                 return True
             else:
                 exp = expressions[0]
                 remaining_exp = expressions[1:]
+                #print "calling"
                 call_result_d = defer.maybeDeferred(self._call_op, self._translate_arg(exp))
                 call_result_d.addCallback(and_cb, remaining_exp)
                 return call_result_d
         return and_cb(True, expressions)
-    ops["&&"] = _op_and
     
     def _op_elof(self, el, el_l):
+        #print "elof", el, el_l
         translated_el = self._translate_arg(el)
         translated_el_l = self._translate_arg(el_l)
         def elof_cb(result):
@@ -114,17 +139,27 @@ class Evaluator():
         dl = defer.DeferredList([translated_el, translated_el_l])
         dl.addCallback(elof_cb)
         return dl
-    ops["E"] = _op_elof
+
+    def _op_equal(self, el1, el2):
+        translated_el1 = self._translate_arg(el1)
+        translated_el2 = self._translate_arg(el2)
+        def equal_cb(result):
+            success, el1 = result[0]
+            success, el2 = result[1]
+            return el1 == el2
+        dl = defer.DeferredList([translated_el1, translated_el2])
+        dl.addCallback(equal_cb)
+        return dl
 
     def _op_true(self, *ignored):
         return True
-    ops["true"] = _op_true
     
     def _op_false(self, *ignored):
         return False
-    ops["false"] = _op_false
 
     def evaluate(self, expression, dynamic_args = None):
+        #print "evaluating"
         self.dynamic_args = dynamic_args if dynamic_args else self.dynamic_args
         r = defer.maybeDeferred(self._call_op, expression)
+        #print "returning r", r
         return r
